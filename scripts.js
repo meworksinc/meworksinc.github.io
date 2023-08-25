0f106a8ac8f88ba8536d7cbe76c9101d776c4123
meworksco <32040708+meworksco@users.noreply.github.com>
2023-08-24 17:06:41 -0700

    Website files

---
+++ scripts.js
@@ -0,0 +1,31 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
+const binary = "01";
+const columns = canvas.width / 15;
+const drops = [];
+
+for (let i = 0; i < columns; i++) {
+    drops[i] = 1;
+}
+
+function drawMatrixRain() {
+    ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#0F0";
+    ctx.font = "15px monospace";
+
+    for (let i = 0; i < drops.length; i++) {
+        const text = binary.charAt(Math.floor(Math.random() * binary.length));
+        ctx.fillText(text, i * 15, drops[i] * 15);
+
+        if (drops[i] * 15 > canvas.height && Math.random() > 0.975) {
+            drops[i] = 0;
+        }
+        drops[i]++;
+    }
+}
+
+setInterval(drawMatrixRain, 70);
